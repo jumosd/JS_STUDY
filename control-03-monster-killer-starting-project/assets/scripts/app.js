3bc6c2ebf0f00c3f392079fc1067958eea7464df
하방방 <81242485+jumosd@users.noreply.github.com>
2025-01-07 12:55:15 +0900

    힐함수추가,endRound()함수추가
    피채워주는 힐함수
    
    endRound는 매판마다 몬스터가 공격하는것

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,21 +1,14 @@
 const ATTACK_VALUE = 10; //플레이어의 공격력 전역적으로 사용할것임 전역적으로 사용하는 상수는 일반적으로 대문자로 활용됨
 const STRONG_ATTACK_VALUE = 20; //강공격 추가
-const MONSTER_ATTACK_VALUE = 14;
+const MONSTER_ATTACK_VALUE = 14; //몬스터 공격력
+const HEAL_VALUE = 20
 let chooseMaxLife = 100; //몬스터와 플레이어의 최대체력임 
 let currentMonsterHealth = chooseMaxLife; //몬스터의 체력을 초기화
 let currentPlayerHealth = chooseMaxLife;  //플레이어의 체력을 초기화
 
 adjustHealthBars(chooseMaxLife); //html에최대 체력바 초기화 하는 함수
 
-function attackMonster(mode) {
-    let maxDamage
-    if (mode === "ATTACK") {
-        maxDamage = ATTACK_VALUE
-    } else if (mode === "STRONG_ATTACK") {
-        maxDamage = STRONG_ATTACK_VALUE
-    }
-    const damage = dealMonsterDamage(maxDamage)
-    currentMonsterHealth -= damage //몬스터체력에서 데미지 깍음
+function endRound() {
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
     currentPlayerHealth -= playerDamage
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
@@ -27,14 +20,39 @@ function attackMonster(mode) {
     }
 }
 
+function attackMonster(mode) {
+    let maxDamage
+    if (mode === "ATTACK") {
+        maxDamage = ATTACK_VALUE
+    } else if (mode === "STRONG_ATTACK") {
+        maxDamage = STRONG_ATTACK_VALUE
+    }
+    const damage = dealMonsterDamage(maxDamage)
+    currentMonsterHealth -= damage //몬스터체력에서 데미지 깍음
+    endRound()
+}
+
 //공격함수
 function attackHandler() {
     attackMonster("ATTACK")
 }
-
+//강공격함수
 function strongAttackHandler() {
     attackMonster("STRONG_ATTACK")
+}
+//힐함수
+function healHandler() {
+    let healValue
+    if (currentPlayerHealth >= chooseMaxLife - HEAL_VALUE) {
+        healValue = chooseMaxLife - currentPlayerHealth
+    } else {
+        healValue = HEAL_VALUE
+    }
+    increasePlayerHealth(healValue)
+    currentPlayerHealth += healValue
+    endRound()
 }
 
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healHandler)
