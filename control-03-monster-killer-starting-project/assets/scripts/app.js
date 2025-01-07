35e486941e43a157be519a3c85314629eeec7ecb
하방방 <81242485+jumosd@users.noreply.github.com>
2025-01-07 12:12:03 +0900

    강한공격추가, 함수리팩토링

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10; //플레이어의 공격력 전역적으로 사용할것임 전역적으로 사용하는 상수는 일반적으로 대문자로 활용됨
+const STRONG_ATTACK_VALUE = 20; //강공격 추가
 const MONSTER_ATTACK_VALUE = 14;
 let chooseMaxLife = 100; //몬스터와 플레이어의 최대체력임 
 let currentMonsterHealth = chooseMaxLife; //몬스터의 체력을 초기화
@@ -6,9 +7,14 @@ let currentPlayerHealth = chooseMaxLife;  //플레이어의 체력을 초기화
 
 adjustHealthBars(chooseMaxLife); //html에최대 체력바 초기화 하는 함수
 
-//공격함수
-function attackHandler() {
-    const damage = dealMonsterDamage(ATTACK_VALUE) // 0~1 * ATTACK_VALUE 값을 데미지로줌 progerss bar의 value를 깍음 AND 데미지를 반환함
+function attackMonster(mode) {
+    let maxDamage
+    if (mode === "ATTACK") {
+        maxDamage = ATTACK_VALUE
+    } else if (mode === "STRONG_ATTACK") {
+        maxDamage = STRONG_ATTACK_VALUE
+    }
+    const damage = dealMonsterDamage(maxDamage)
     currentMonsterHealth -= damage //몬스터체력에서 데미지 깍음
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
     currentPlayerHealth -= playerDamage
@@ -21,4 +27,14 @@ function attackHandler() {
     }
 }
 
-attackBtn.addEventListener('click', attackHandler)
+//공격함수
+function attackHandler() {
+    attackMonster("ATTACK")
+}
+
+function strongAttackHandler() {
+    attackMonster("STRONG_ATTACK")
+}
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
