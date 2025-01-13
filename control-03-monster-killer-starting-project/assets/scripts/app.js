2faef9a5e3b61ca77edcecb53c07e38f88c120d4
하방방 <81242485+jumosd@users.noreply.github.com>
2025-01-13 12:54:35 +0900

    보너스생명사용

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -5,12 +5,23 @@ const HEAL_VALUE = 20
 let chooseMaxLife = 100; //몬스터와 플레이어의 최대체력임 
 let currentMonsterHealth = chooseMaxLife; //몬스터의 체력을 초기화
 let currentPlayerHealth = chooseMaxLife;  //플레이어의 체력을 초기화
+let hasBonusLife = true
 
 adjustHealthBars(chooseMaxLife); //html에최대 체력바 초기화 하는 함수
 
 function endRound() {
+    let initialPlayerHealth = currentPlayerHealth
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
     currentPlayerHealth -= playerDamage
+
+    if (currentPlayerHealth <= 0 && hasBonusLife) {
+        hasBonusLife = false
+        removeBonusLife()
+        currentPlayerHealth = initialPlayerHealth + 20
+        alert("보너스 생명 사용 !!")
+        setPlayerHealth(initialPlayerHealth + 20)
+    }
+
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert("이겼다!!")
     } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
